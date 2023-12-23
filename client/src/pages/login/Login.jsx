cdf04ce42d99523c30fa61ce11f5470143a9f14b
Malith Iroshan <99582850+IT21229084@users.noreply.github.com>
2023-12-23 14:15:39 +0530

    Login Page is Done

---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./login.css"
+import { useState } from 'react'
+import { useContext } from 'react'
+import { AuthContext } from "../../context/AuthContext"
+import axios from "axios"
+const Login = () => {
+    const [credentials, setCredentials] = useState({
+        username: undefined,
+        password: undefined
+    })
+
+    const {user, loading, error, dispatch } = useContext(AuthContext)
+    const handleChange = (e) => {
+        setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }))
+    }
+
+    const handleClick = async (e) => {
+        e.preventDefault()
+        dispatch({ type: "LOGIN_START" })   
+        try {
+            const res = await axios.post("/auth/login",credentials)
+            dispatch({ type: "LOGIN_SUCCESS", payload: res.data })
+        } catch (error) {
+            dispatch({ type: "LOGIN_FAILURE", payload: error.response.data })
+        }
+    }
+console.log(user)
+    return (
+        <div className="login">
+            <div className="lContainer">
+                <input type='text' placeholder='username' id='username' onChange={handleChange} className='lInput' />
+                <input type='password' placeholder='password' id='password' onChange={handleChange} className='lInput' />
+                <button onClick={handleClick} className="lButoon">Login</button>
+                {error && <span>{error.message}</span>}
+            </div>
+        </div>
+    )
+}
+
+export default Login
