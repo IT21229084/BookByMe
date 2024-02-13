237cde27fe1c1da3ef17520dc2977eeee4cbb813
Malith Iroshan <99582850+IT21229084@users.noreply.github.com>
2024-02-14 01:53:18 +0530

    Update index.js
    testing purpose

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { SearchContextProvider } from './context/SearchContext';
 import { AuthContextProvider } from './context/AuthContext';
-
+//Test
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
